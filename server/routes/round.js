dcc48be9927540670314d6dc663560173c4fb93a
jfelici5 <jfelici5@u.rochester.edu>
2020-07-31 12:46:35 -0700

    routes for round saving

---
+++ server/routes/round.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const { Round } = require("../models/Round");
+
+
+const { auth } = require("../middleware/auth");
+
+//=================================
+//             User
+//=================================
+
+router.post("/uploadRound", auth, (req, res) => {
+    
+    const round = new Round(req.body)
+
+    round.save((err) => {
+        if(err) return res.status(400).json({success: false, err})
+        return res.status(200).json({success: true})
+    })
+
+});
+
+
+module.exports = router;
