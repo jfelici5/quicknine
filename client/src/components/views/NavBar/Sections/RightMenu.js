31b4b214671a81c49d5d3c66e8c25923049b5b93
jfelici5 <jfelici5@u.rochester.edu>
2020-07-30 22:35:20 -0700

    added route for round upload

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -33,6 +33,11 @@ function RightMenu(props) {
   } else {
     return (
       <Menu mode={props.mode}>
+        <Menu.Item key="upload">
+          <a href = "/upload">
+            Upload
+          </a>
+        </Menu.Item>
         <Menu.Item key="logout">
           <a onClick={logoutHandler}>Logout</a>
         </Menu.Item>
---
+++ client/src/components/views/UploadRoundPage/UploadRoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UploadRoundPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UploadRoundPage
