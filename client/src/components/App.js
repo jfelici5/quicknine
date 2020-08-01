984542808990e6aa3433d2abfc03c624d0b5b2fc
jfelici5 <jfelici5@u.rochester.edu>
2020-07-31 21:55:45 -0700

    taking navbar off landing page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,7 @@ import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
 import UploadRoundPage from "./views/UploadRoundPage/UploadRoundPage";
+import UserHomePage from "./views/UserHomePage/UserHomePage"
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -16,16 +17,13 @@ import UploadRoundPage from "./views/UploadRoundPage/UploadRoundPage";
 function App() {
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
-      <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/user" component={Auth(UserHomePage, true)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/upload" component={Auth(UploadRoundPage, true)} />
         </Switch>
-      </div>
-      <Footer />
     </Suspense>
   );
 }
